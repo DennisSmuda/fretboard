809db1f8b065196c450bd86a5f7f09eb6cc79638
Dennis Smuda <dennissmuda@msn.com>
2017-03-19 19:46:01 +0100

    Added tonal-fret

--- src/js/fretboard.js
+++ src/js/fretboard.js
@@ -1,12 +1,18 @@
 import tonal from 'tonal';
+let fret = require('tonal-fretboard');
 
 /**
  * Fretboard
+ * @property numFrets 
+ * @property numStrings 
+ * @property tuning 
  */
 export default class Fretboard {
   constructor({
     numFrets = 15,
-    numStrings = 6
+    numStrings = 6,
+    tuning = 'guitar',
+    scale = 'Eb bebop'
   }) {
     this.container = document.getElementById('canvas-container');
     this.canvas = document.getElementById('fretboard');
@@ -15,11 +21,21 @@ export default class Fretboard {
     this.canvas.height = this.container.clientHeight;
     this.width = this.canvas.clientWidth;
     this.height = this.canvas.clientHeight;
+    this.tuning = tuning;
+    // Get Scale Notes from tonal
+    this.scale = tonal.scale(scale);
 
+    // Frets
     this.numFrets = numFrets;
     this.zeroFretOffset = 15;
     this.fretWidth = (this.width - this.zeroFretOffset) / this.numFrets;
+
+    // Strings
     this.numStrings = numStrings;
+
+    // Notes
+    this.numNotes = this.numFrets * this.numStrings;
+    this.notes = [this.numNotes];
 
     this.init();
   }
@@ -30,8 +46,18 @@ export default class Fretboard {
   init() {
     this.drawFrets();
     this.drawStrings();
+    this.initNotes();
 
-    console.log(tonal.scale('Bb lydian'));
+  }
+
+  initNotes() {
+    let board = fret.notes('guitar', 0, this.numFrets, this.scale);
+    for (let string of board) {
+      console.log('String:')
+      for (let note of string) {
+        console.log(note)
+      }
+    }
   }
 
   drawFrets() {
@@ -47,12 +73,12 @@ export default class Fretboard {
 
   drawStrings() {
     let stringPadding = this.height / 10;
-    let height = this.height - stringPadding ;
+    let height = this.height - stringPadding;
     let stringHeight = height / this.numStrings;
     this.ctx.strokeStyle = '#c0c0c0'
 
     for (let i = 0; i < this.numStrings; i++) {
-      console.log(`Draw String: ${i * stringHeight} ` + i * this.fretWidth + ':' + i);
+      // console.log(`Draw String: ${i * stringHeight} ` + i * this.fretWidth + ':' + i);
       this.ctx.beginPath();
       this.ctx.moveTo(0, i * stringHeight + stringPadding);
       this.ctx.lineTo(this.width, i * stringHeight + stringPadding);
